d12ed4d0a9cbf348ff7fae12d34edec2bcd40fd7
LOUSSIN André <105634694+andreloussin@users.noreply.github.com>
2026-05-07 10:13:53 +0100

    feat(logging): integrate Pino logger with NestJS

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/env.validation';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: envValidationSchema,
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: { singleLine: true },
+              }
+            : undefined,
+      },
     }),
   ],
   controllers: [AppController],
